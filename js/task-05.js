f48f4674782ea9e4ff418c3161090df8e597c99c
Bigtoxa766 <bigtoxa766@gmail.com>
2023-02-25 12:23:46 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,13 @@
-const inputEl = document.querySelector('#name-input');
-const outputEl = document.querySelector('#name-output');
 
-inputEl.addEventListener('input', event => {
-  outputEl.textContent = event.target.value
-})
+const inputEl = document.querySelector('#name-input')
+const outputEl = document.querySelector('#name-output')
+
+const handleNameGreet = (e) => {
+  if (e.target.value.trim() === '') {
+    outputEl.textContent = 'Anonymous'
+  } else {
+    outputEl.textContent = e.target.value;
+  }
+}
+
+inputEl.addEventListener('input', handleNameGreet)
