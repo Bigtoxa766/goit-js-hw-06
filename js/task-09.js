55b0c693e14fdd5167d13569a39c33bec7df12c9
Bigtoxa766 <bigtoxa766@gmail.com>
2023-02-26 12:34:39 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body') 
+const colorNameEl = document.querySelector('.color')
+const btnEl = document.querySelector('.change-color')
+
+
+btnEl.addEventListener('click', () => {
+ 
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorNameEl.textContent = getRandomHexColor();
+})
+
