451a0b0f05db6233148612b0a06c1581dc64eaeb
Bigtoxa766 <bigtoxa766@gmail.com>
2023-02-27 02:11:25 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -4,6 +4,29 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-
 const collectionEl = document.querySelector('#controls')
+const btnCreateEl = document.querySelector('button[data-create]')
+const btnDestroyEl = document.querySelector('button[data-destroy]')
+const boxesEl = document.querySelector('#boxes')
 
+btnDestroyEl.addEventListener('click', () => (boxesEl.innerHTML = ""))
+btnCreateEl.addEventListener('click', createBoxes)
+
+
+function createBoxes() {
+  let amount = collectionEl.firstElementChild.value;
+  let defaultSize = 30;
+
+  for (let i = 0; i < amount; i+=1) {
+    let boxSize = i * 10 + defaultSize;
+   
+    const markup = `<div class="box" 
+    style="background:${getRandomHexColor()}"; 
+    width:${boxSize}px;
+    height; ${boxSize}px></div>`;
+
+    boxesEl.insertAdjacentHTML('afterbegin', markup)
+  }
+  
+}
+  
