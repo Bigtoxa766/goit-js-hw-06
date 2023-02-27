83df287bc254d99d9dee29fa83edaa1d311f08ec
Bigtoxa766 <bigtoxa766@gmail.com>
2023-02-27 18:16:25 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,11 +2,11 @@
 const inputEl = document.querySelector('#validation-input');
 
 inputEl.addEventListener('blur', (e) => {
-  if (e.target.value.trim().length < e.target.dataset.length) {
-   e.target.style.borderColor = 'red';
+  if (e.target.value.trim().length === Number(inputEl.getAttribute('data-length'))) {
+   e.target.style.borderColor = 'green';
   } else {
-    e.target.style.borderColor = 'green';
+    e.target.style.borderColor = 'red';
   }
 })
 
-console.log(inputEl);
+
